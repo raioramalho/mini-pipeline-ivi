15dc54301468d571d16decd2551640796e76eb26
Alan Ramalho <ramalho.sit@gmail.com>
2025-06-20 16:17:05 -0300

    Remover configurações obsoletas do Terraform e adicionar nova configuração de implantação do Kubernetes

---
+++ terraform/k8s/dev/variables.tf
@@ -0,0 +1,15 @@
+variable "kubernetes_config_path" {
+  type        = string
+  description = "Path to the Kubernetes config file"
+  default     = "~/.kube/local"
+}
+
+variable "kubernetes_token" {
+  type = string
+}
+
+variable "tls_insecure" {
+  type        = bool
+  description = "Use TLS for Kubernetes provider"
+  default     = false
+}
--- terraform/openstack/main.tf
+++
@@ -1,5 +0,0 @@
-provider "openstack" {}
-
-resource "openstack_objectstorage_container_v1" "csv" {
-  name = "csv"
-}
